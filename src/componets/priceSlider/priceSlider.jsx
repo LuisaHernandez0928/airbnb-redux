4ab6d80cf1b5fca73d8815d73d4b09bdca878126
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-02-19 22:36:48 -0500

    removed useState

--- src/componets/priceSlider/priceSlider.jsx
+++ src/componets/priceSlider/priceSlider.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { getMinPrice, getMaxPrice } from "../../reducer/airbnbsSlice";
 import styles from "./index.module.css";
